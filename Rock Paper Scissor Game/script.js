5e28debbcb96181544517f42742440bdbd29e20a
Aakash <aakashkumarsumanaksgi7@gmail.com>
2024-06-23 08:06:30 +0530

    made function for each Choice

--- Rock Paper Scissor Game/script.js
+++ Rock Paper Scissor Game/script.js
@@ -2,8 +2,7 @@ let rock = document.querySelector("#rock");
 let paper = document.querySelector("#paper");
 let scissor = document.querySelector("#scissor");
 let result = document.querySelector("h3");
-console.log(result.textContent);
-rock.addEventListener("click", function(evt){
+function choiceRock(){
     let randomNumber = Math.floor(Math.random()*3) + 1;
     if(randomNumber == 1){
         result.textContent = "Tie😕";
@@ -15,30 +14,39 @@ rock.addEventListener("click", function(evt){
         result.textContent = "You Win😍";
         result.style.color = "lightGreen";
     }
+}
+function choicePaper(){
+    let randomNumber = Math.floor(Math.random()*3) + 1;
+    if(randomNumber == 1){
+        result.textContent = "You Win😍";
+        result.style.color = "lightGreen";
+    }else if(randomNumber == 2){
+        result.textContent = "Tie😕";
+        result.style.color = "Yellow";
+    }else if(randomNumber == 3){
+        result.textContent = "You Lose😔";
+        result.style.color = "red";
+    }
+}
+function choiceScissor(){
+    let randomNumber = Math.floor(Math.random()*3) + 1;
+    if(randomNumber == 1){
+        result.textContent = "You Lose😔";
+        result.style.color = "red";
+    }else if(randomNumber == 2){
+        result.textContent = "You Win😍";
+        result.style.color = "lightGreen";
+    }else if(randomNumber == 3){
+        result.textContent = "Tie😕";
+        result.style.color = "Yellow";
+    }
+}
+rock.addEventListener("click", function(evt){
+    choiceRock();
 })
 paper.addEventListener("click", function(evt){
-    let randomNumber = Math.floor(Math.random()*3) + 1;
-    if(randomNumber == 1){
-        result.textContent = "You Win😍";
-        result.style.color = "lightGreen";
-    }else if(randomNumber == 2){
-        result.textContent = "Tie😕";
-        result.style.color = "Yellow";
-    }else if(randomNumber == 3){
-        result.textContent = "You Lose😔";
-        result.style.color = "red";
-    }
+    choicePaper();
 })
 scissor.addEventListener("click", function(evt){
-    let randomNumber = Math.floor(Math.random()*3) + 1;
-    if(randomNumber == 1){
-        result.textContent = "You Lose😔";
-        result.style.color = "red";
-    }else if(randomNumber == 2){
-        result.textContent = "You Win😍";
-        result.style.color = "lightGreen";
-    }else if(randomNumber == 3){
-        result.textContent = "Tie😕";
-        result.style.color = "Yellow";
-    }
+    choiceScissor();
 })
