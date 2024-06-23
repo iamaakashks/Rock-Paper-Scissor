1fe09648686b7ab1063507fb0b6e91aae8147982
Aakash <aakashkumarsumanaksgi7@gmail.com>
2024-06-23 09:44:23 +0530

    updated the score button

--- Rock Paper Scissor Game/script.js
+++ Rock Paper Scissor Game/script.js
@@ -2,17 +2,26 @@ let rock = document.querySelector("#rock");
 let paper = document.querySelector("#paper");
 let scissor = document.querySelector("#scissor");
 let result = document.querySelector("h3");
+let wins = document.querySelector("#wins")
+let loses = document.querySelector("#loses")
+let tie = document.querySelector("#tie")
+const score = {
+    wins: 0, loses: 0, tie:0
+};
 function choiceRock(){
     let randomNumber = Math.floor(Math.random()*3) + 1;
     if(randomNumber == 1){
         result.textContent = "Tie😕";
         result.style.color = "Yellow";
+        score.tie += 1;
     }else if(randomNumber == 2){
         result.textContent = "You Lose😔";
         result.style.color = "red";
+        score.loses += 1;
     }else if(randomNumber == 3){
         result.textContent = "You Win😍";
         result.style.color = "lightGreen";
+        score.wins += 1;
     }
 }
 function choicePaper(){
@@ -20,12 +29,15 @@ function choicePaper(){
     if(randomNumber == 1){
         result.textContent = "You Win😍";
         result.style.color = "lightGreen";
+        score.wins += 1;
     }else if(randomNumber == 2){
         result.textContent = "Tie😕";
         result.style.color = "Yellow";
+        score.tie += 1;
     }else if(randomNumber == 3){
         result.textContent = "You Lose😔";
         result.style.color = "red";
+        score.loses += 1;
     }
 }
 function choiceScissor(){
@@ -33,20 +45,32 @@ function choiceScissor(){
     if(randomNumber == 1){
         result.textContent = "You Lose😔";
         result.style.color = "red";
+        score.loses += 1;
     }else if(randomNumber == 2){
         result.textContent = "You Win😍";
         result.style.color = "lightGreen";
+        score.wins += 1;
     }else if(randomNumber == 3){
         result.textContent = "Tie😕";
         result.style.color = "Yellow";
+        score.tie += 1;
     }
 }
 rock.addEventListener("click", function(evt){
     choiceRock();
+    wins.innerText = `Wins: ${score.wins}`;
+    loses.innerText = `Loses: ${score.loses}`;
+    tie.innerText = `Tie: ${score.tie}`;
 })
 paper.addEventListener("click", function(evt){
     choicePaper();
+    wins.innerText = `Wins: ${score.wins}`;
+    loses.innerText = `Loses: ${score.loses}`;
+    tie.innerText = `Tie: ${score.tie}`;
 })
 scissor.addEventListener("click", function(evt){
     choiceScissor();
+    wins.innerText = `Wins: ${score.wins}`;
+    loses.innerText = `Loses: ${score.loses}`;
+    tie.innerText = `Tie: ${score.tie}`;
 })
